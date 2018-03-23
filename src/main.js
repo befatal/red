4a1242c1769ad948aba1484553c9c2da1201f124
Guest User <Guest@Epicodus-3D.local>
2018-03-23 11:29:13 -0700

    add styling to index page with interaction from main.js doctor search function.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import './styles.css';
 import { DoctorApi } from './doctor-interface.js';
 
 $(document).ready(function() {
-  $('#doctorLocation').click(function() {
+  $('#doctorSearch').click(function() {
     event.preventDefault();
     let parameters = {'name' , 'issue'};
     let doctor = new DoctorApi (parameters);
