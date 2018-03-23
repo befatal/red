419d52d0b9a45afa50c33a31f3671b40e38026cf
Guest User <Guest@Epicodus-3D.local>
2018-03-23 11:27:16 -0700

    add doctor.get doctor functionality along with error message if search has an error.

--- src/main.js
+++ src/main.js
@@ -9,4 +9,10 @@ $(document).ready(function() {
     event.preventDefault();
     let parameters = {'name' , 'issue'};
     let doctor = new DoctorApi (parameters);
-  })
+    doctor.getDoctor().then(function(response) {
+      let body = JSON.parse(response);
+    }, function(error) {
+      $('.errors').text(`There was an error processing your request: ${error.message}`);
+      });
+    });
+  });
