14eec35ea97a45c1b55925a49740f87234eb2e66
Guest User <Guest@Epicodus-3D.local>
2018-03-23 12:36:15 -0700

    working towards fixing error of name and speciality not showing up.

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -1,18 +1,16 @@
-export class DoctorApi{
+export class DoctorApi {
   constructor(parameters){
     this.location = 'or-portland';
     this.name = parameters.name;
-    this.issue = parameters.issue;
+    this.speciality = parameters.speciality;
   }
 
   getDoctor(doctor) {
-   console.log(doctor)
 
    let request = new XMLHttpRequest();
    let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=53655e2582ea8e82e621ce1990e85a45'
    return new Promise(function(resolve, reject) {
      request.onload = function() {
-       console.log(resolve);
        if (this.status === 200) {
          resolve(request.response);
        } else {
@@ -21,7 +19,6 @@ export class DoctorApi{
      };
      request.open("GET", url, true);
      request.send();
-     console.log(resolve);
    });
    };
  }
--- src/main.js
+++ src/main.js
@@ -7,12 +7,23 @@ import { DoctorApi } from './doctor-interface.js';
 $(document).ready(function() {
   $('#doctorSearch').click(function() {
     event.preventDefault();
-    let parameters = {'name' , 'issue'};
-    let doctor = new DoctorApi (parameters);
-    doctor.getDoctor().then(function(response) {
-      let body = JSON.parse(response);
-    }, function(error) {
+    let parameters = {'name':'', 'speciality':''};
+    let data = new DoctorApi (parameters);
+    let promise = data.getDoctor();
+    console.log(data)
+
+  promise.then(function(response) {
+    let body = JSON.parse(response);
+    let doctorArray =[];
+    body.data.forEach(function(element) {
+      doctorArray.push(element.name + " " + element.specialties);
+    },
+    doctorArray.forEach(function(element) {
+      $(".showDoctor").append("<p>" + element + "</p>");
+    }),
+      function(error) {
       $('.errors').text(`There was an error processing your request: ${error.message}`);
       });
     });
   });
+})
