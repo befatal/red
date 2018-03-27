c7c7460390af9974bc423a0603a93d52ee8137fa
Alexanderficklin <a.ficklin11@gmail.com>
2018-03-26 20:20:47 -0700

    add error function to doctor-interface.js and name and issue function.

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 export class DoctorApi {
   constructor() {
     this.apiKey = process.env.exports.apiKey;
@@ -5,20 +6,33 @@ export class DoctorApi {
   }
 
 
-  getDoctor() {
-
-   let request = new XMLHttpRequest();
-   let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=53655e2582ea8e82e621ce1990e85a45'
-   return new Promise(function(resolve, reject) {
-     request.onload = function() {
-       if (this.status === 200) {
-         resolve(request.response);
-       } else {
-         reject(Error(request.statusText));
-       }
-     };
-     request.open("GET", url, true);
-     request.send();
-   });
-   };
+  getDoctorIssue(issue) {
+    $.ajax({
+      url: "https://api.betterdoctor.com/2016-03-01/practices?name=${issue}&location=45.523%2C-122.676%2C100&user_location=45.523%2C-122.676&skip=0&limit=10&user_key=${this.apiKey}",
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        searchDoctors(response);
+      },
+        error: function() {
+          $('#errors').text("Sorry, no results found");
+        }
+      })
  }
+ getDoctor(name) {
+   $.ajax({
+     url: "https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.523%2C-122.676%2C100&user_location=45.523%2C-122.676&sort=full-name-asc&skip=0&limit=10&user_key=${this.apiKey}",
+     type: 'GET',
+     data: {
+       format: 'json'
+     },
+     success: function(response) {
+       searchDoctors(response);
+     },
+       error: function() {
+         $('#errors').text("Sorry, no results found");
+       }
+     })
+}
