43c9d188a810b83704d316fb13b46337f54d3689
Guest User <Guest@Epicodus-5B.local>
2018-03-28 15:46:38 -0700

    delete a search doctor function in main.js and begin showing it.

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import './styles.css';
 import { DoctorApi } from './doctor-search.js';
 
 function searchDoctors(response){
-  for (var i = 0; i < 10; i++) {
+  forEach(function(response) (var i = 0; i < 10; i++) {
     let name = response.data[i].profile.first_name + ' ' + response.data[i].last_name;
     let address = response.data[i].practices[0].visit_address.street + ' ' + response.data[i].visit_address.city + ' ' + response.data[i].visit_address.state;
     let acceptPatients;
@@ -15,24 +15,13 @@ function searchDoctors(response){
       acceptPatients = "No";
     }
 
-  function searchDoctorsIssue(response){
-    for (var i = 0; i < 10; i++) {
-      let name = response.data[i].profile.first_name + ' ' + response.data[i].last_name;
-      let address = response.data[i].practices[0].visit_address.street + ' ' + response.data[i].visit_address.city + ' ' + response.data[i].visit_address.state;
-      let acceptPatients;
-      if reponse.accepts_new_patients == true) {
-        acceptPatients = "Yes";
-      } else {
-        acceptPatients = "No";
-        }
-   }
-  }
-
-
 $(document).ready(function() {
     let search = newDoctor.getDoctor();
     $('#doctorSearch').click(function() {
-
+      event.preventDefault();
+      $('.showDoctor').empty()
+      let doctor = $('#doctor').val();
+      $('#showDoctor').text(doctor)
     });
 
   });
